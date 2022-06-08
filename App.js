4eeec63d552cdfbd903dbf3dc665822c4800fb47
unknown <kweyz@daum.net>
2022-06-08 23:48:42 +0900

    Feat: webView source url

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
+
+const YOUTUBE = "https://www.youtube.com/";
+const GOOGLE = "https://www.google.com/";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={{ width: "80%", height: "80%" }}>
+        <WebView source={{ uri: YOUTUBE }} onLoad={console.log("Loaded!")} />
+      </View>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -12,9 +19,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 10,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
